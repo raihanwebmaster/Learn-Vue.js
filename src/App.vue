ee729ef90cfd625b41c0681dd714a30cd18406e2
raihanwebmaster <raihanemon2015@gmail.com>
2022-06-07 07:35:17 +0200

    methods

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,8 @@
-<!---------------------- List Rendering ------------------------->
+<!---------------------- Methods ------------------------->
 <template>
-  <!-- <h2 v-for="name in names" :key="name">{{name}}</h2> -->
-  <h2 v-for="(name, index) in names" :key="name"> {{ index }} {{ name }}</h2>
-
-  <h2 v-for="name in fullNames" :key="name.first"> {{ name.first }} {{ name.last }}</h2>
-
-  <div v-for="actor in actors" :key="actor.name">
-    <h2>{{ actor.name }}</h2>
-    <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
-  </div>
-
-  <h2 v-for="(value, key, index) in myInfo" :key="value">{{index}} {{key}} {{value}}</h2>
-
-  <template v-for="name in names" :key="name">
-    <h2>{{name}}</h2>
-    <hr>
-  </template>
-
-  <template v-for="name in names" :key="name" >
-    <h2 v-if="name === 'Bruce'">{{name}}</h2>
-  </template>
+  <h2>{{ 2 + 3 + 5 }}</h2>
+  <h2>Add method - {{ add(2, 5, 3) }}</h2>
+  <h2>Multiple method - {{multiply(10)}}</h2>
 </template>
 
 
@@ -29,31 +12,17 @@ export default {
   name: "App",
   data() {
     return {
-      names: [
-        'Bruce', 'Clark', 'Diana'
-      ],
-      fullNames: [
-        { first: 'Bruce', last: 'Wayne' },
-        { first: 'Clark', last: 'kent' },
-        { first: 'Princess', last: 'Diana' }
-      ],
-      actors: [
-        {
-          name: 'Christian Bale',
-          movies: ['Batman', 'The Prestige'],
-        },
-        {
-          name: 'Di Caprio',
-          movies: ['Titanic', 'Inception'],
-        },
-      ],
-      myInfo: {
-        name: 'Raihan',
-        gitHub: 'raihanwebmaster',
-        course: 'Vue 3'
-      }
+      baseMultiplier: 5
     };
   },
+  methods: {
+    add(a, b, c) {
+      return a + b + c
+    },
+    multiply(num) {
+      return num* this.baseMultiplier
+    }
+  }
 };
 </script>
 
