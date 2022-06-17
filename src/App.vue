806d86a1ea6b0db2c2aac76cb5b44ef2d3b3ba82
raihanwebmaster <raihanemon2015@gmail.com>
2022-06-17 20:59:53 +0200

    v-pre and v-once

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,12 @@
-<!---------------------- Modifiers ------------------------->
- <!-- A suffix you can add to either the v-on direactive or the v-model directive to add some functionality inline within the template -->
+<!---------------------- v-once v-pre ------------------------->
+<!-- v-once -> 
+    Renders the corresponding HTML  element only once . On subsequent re-renders, the element is treated as static content and skipped. -->
+<!-- v-pre -> 
+  Skips compilation for the corresponding element -->
 <template>
-  <div>
-    <pre>
-    {{ JSON.stringify(formValues, null, 2) }}
-  </pre>
-  </div>
-  <form @submit.prevent="submitForm">
-    <div>
-      <label for="name">Name</label>
-      <input type="text" id="name" v-model.trim.lazy="formValues.name">
-    </div>
-
-
-    <div>
-      <label for="profile">Profile Summary</label>
-      <textarea id="profile" v-model="formValues.profileSummary" />
-      <!-- <textarea name="" id="profile" cols="30" rows="10" v-model="formValues.profileSummary"></textarea> -->
-    </div>
-
-
-    <div>
-      <label for="country">Country</label>
-      <select id="country" v-model="formValues.country">
-        <option value="">Select a Country</option>
-        <option value="india">India</option>
-        <option value="vietnam">Vietnam</option>
-        <option value="singapore">Singapore</option>
-      </select>
-    </div>
-
-
-
-    <div>
-      <label for="job-location">Job Location</label>
-      <select id="job-location" multiple v-model="formValues.jobLocation">>
-        <option value="india">India</option>
-        <option value="vietnam">Vietnam</option>
-        <option value="singapore">Singapore</option>
-      </select>
-    </div>
-
-
-
-    <div>
-      <input type="checkbox" id="remoteWork" v-model="formValues.remoteWork" true-value="yes" false-value="no">
-      <label for="remoteWork">Open to remote work?</label>
-    </div>
-
-
-
-    <div>
-      <label>Skill set</label>
-      <input type="checkbox" id=" html" value="html" v-model="formValues.skillSet" />
-      <label for="html">HTML</label>
-      <input type="checkbox" id="css" value="css" v-model="formValues.skillSet" />
-      <label for="css">CSS</label>
-      <input type="checkbox" id="javascript" value="javascript" v-model="formValues.skillSet" />
-      <label for="javascript">JavaScript</label>
-    </div>
-
-
-    <div>
-      <label> Years of Experinece</label>
-      <input type="radio" id="0-2" value="0-2" v-model="formValues.yearsOfExperience">
-      <label for="0-2">0-2</label>
-      <input type="radio" id="3-5" value="3-5" v-model="formValues.yearsOfExperience">
-      <label for="3-5">3-5</label>
-      <input type="radio" id="6-10" value="6-10" v-model="formValues.yearsOfExperience">
-      <label for="6-10">6-10</label>
-      <input type="radio" id="10+" value="10+" v-model="formValues.yearsOfExperience">
-      <label for="10+">10+</label>
-    </div>
-
-
-
-    <div>
-      <label for="age">Age</label>
-      <input @keyup.enter="submitForm" type="number" id="age" v-model.number="formValues.age" />
-    </div>
-
-
-
-    <!-- <div>
-      <button>Submit</button>
-    </div> -->
-  </form>
+    <h2 v-once>{{name}}</h2>
+    <button @click="name = 'uddin'">Change Name</button>
+    <h2 v-pre>{{name}}</h2>
 </template>
 
 
@@ -95,23 +16,10 @@ export default {
   name: "App",
   data() {
     return {
-      formValues: {
-        name: '',
-        profileSummary: '',
-        country: '',
-        jobLocation: [],
-        remoteWork: "no",
-        skillSet: [],
-        yearsOfExperience: '',
-        age:null,
-      }
+      name: 'raihan'
     };
   },
   methods: {
-    submitForm() {
-      // event.preventDefault();
-      console.log('from values', this.formValues)
-    }
   }
 };
 </script>
@@ -126,30 +34,5 @@ export default {
   margin-top: 60px;
 }
 
-label {
-  font-weight: bold;
-  display: flex;
-  margin-bottom: 5px;
-}
 
-input+label {
-  font-weight: bold;
-  display: inline-flex;
-  margin-right: 20px;
-}
-
-input[type='text'],
-textarea,
-select {
-  display: block;
-  width: 400px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.42857143;
-  color: #555;
-  background-color: #fff;
-  background-image: none;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
 </style>
